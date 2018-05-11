ec693822d4b8599dd36a54fc255f1e549023f813
Mr Booi <ayabongabooi2@gmail.com>
2018-05-11 15:51:35 +0200

    changes

--- js/shoe_catalogue.js
+++ js/shoe_catalogue.js
@@ -3,6 +3,10 @@ var colorElem = document.querySelector('.shoeColor');
 var sizeElem = document.querySelector('.shoeSize');
 var stockQtyElem = document.querySelector('.shoeQty');
 var addStockBtn = document.querySelector('.btnStock');
+var shoeSelect = document.querySelector('.shoe');
+var colorSelect = document.querySelector('.shoeColors');
+var ShoeSizeSelect = document.querySelector('.shoeSizes');
+
 
 function ShoeCatalouge(storedItems, trolley) {
   var shoeBrand = '';
@@ -42,10 +46,39 @@ function ShoeCatalouge(storedItems, trolley) {
   function stockMap() {
     return storeShoeStock;
   }
+   
+  function filterShoes(brandName,brandColor,brandSize){
+    var getFilteredItems = []
+
+
+   
+    
+   if(brandName !==''&& brandColor!==''&& brandColor!==''){
+    getFilteredItems= _.filter(storeShoeStock,{"shoeBrand":brandName,"colour":brandColor,"sizeShoe":brandSize});
+    // storeShoeStock.filter(current => current.shoeBrand===brandName)
+      return getFilteredItems
+    }
+    // else   if(brandName !==''&& brandColor!==''){
+    //   getFilteredItems= _.filter(storeShoeStock,{shoeBrand:brandName,colour:brandColor}) ;           
+    //   // storeShoeStock.filter(current => current.shoeBrand===brandName)
+    //     return getFilteredItems;
+    //   }
+
+    // if(brandName !==''){
+    //   getFilteredItems= _.filter(storeShoeStock,{shoeBrand:brandName});
+    //   return getFilteredItems;
+    // }
+
+
+
+    
+  }
+
+
+
 
   function addNewStock(brand, Color, Qty, shoesize, Price) {
-    
-    
+       
      storeShoeStock.push({
       'shoeBrand':brand,
       'colour':Color,
@@ -60,7 +93,8 @@ function ShoeCatalouge(storedItems, trolley) {
 
   return {
     stockadd: addNewStock,
-    storeMap: stockMap
+    storeMap: stockMap,
+    filterBy: filterShoes
   }
 }
 
@@ -70,7 +104,6 @@ var StoredItems = localStorage.getItem('shoppingBasket') ? JSON.parse(localStora
 var shoe_Catalogue = ShoeCatalouge(StoredItems);
 
 function addStock() {
-   
   shoe_Catalogue.stockadd(
     brandElem.value,
     colorElem.value,
@@ -80,8 +113,28 @@ function addStock() {
     );
 
   localStorage.setItem('shoppingBasket', JSON.stringify(shoe_Catalogue.storeMap()));
-
 }
+
+// function selectShoe(){
+
+//    console.log(shoe_Catalogue.filterBy(shoeSelect.value));
+// }
+
+//  function colorSelect(){
+//   shoe_Catalogue.filterBy(colorSelect.value);
+//  } 
+//  function ShoeSizeSelect(){
+//   shoe_Catalogue.filterBy(ShoeSizeSelect.value);
+//  }
 
 
 addStockBtn.addEventListener('click', addStock);
+
+ShoeSizeSelect.addEventListener('change',function(){
+
+console.log(shoe_Catalogue.filterBy(shoeSelect.value))
+});
+
+
+
+
