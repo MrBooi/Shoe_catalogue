4bddb78fd59a719adb821d12a8ddc16c64277013
Mr Booi <ayabongabooi2@gmail.com>
2018-05-14 16:18:37 +0200

    test for add and filter function

--- js/shoe_catalogue.js
+++ js/shoe_catalogue.js
@@ -17,103 +17,81 @@ function ShoeCatalouge(storedItems) {
   var storeShoeStock = storedItems || [];
   var trolley = []
 
-  function setShoeBrand(value) {
-    if (value != undefined) {
-      shoeBrand = value;
-      return shoeBrand;
-    }
-  }
-  function setShoeColor(value) {
-    if (value != undefined) {
-      shoeColor = value;
-      return shoeColor;
-    }
-
-  }
-  function setShoeQty(value) {
-    if (value != undefined) {
-      shoeQty = value;
-      return shoeQty;
-    }
-
-  }
-4
-  function setShoeSize(value) {
-    if (value != undefined) {
-      shoeSize = value;
-      return shoeSize;
-    }
-  }
 
   function stockMap() {
     return storeShoeStock;
   }
-   
-  function filterShoes(SearchItems){
-    return _.filter(storeShoeStock,SearchItems);
+
+  function filterShoes(SearchItems) {
+    trolley = _.filter(storeShoeStock, SearchItems)
+    return _.filter(storeShoeStock, SearchItems);
   }
-   
+
+  function getSearchedItems() {
+    return trolley;
+  }
 
 
 
   // if(getFilteredItems !=={}){
-    //     trolley=  getFilteredItems;
-    //  console.log(Object.assign(trolley));
-    //   }
+  //     trolley=  getFilteredItems;
+  //  console.log(Object.assign(trolley));
+  //   }
 
-  function getTrolley(){
+  function getTrolley() {
     return trolley;
   }
-  function addCart(storeShoeStock){
+  function addCart(storeShoeStock) {
     console.log(storeShoeStock)
-   trolley.map(items=>{
-    if(items.shoeBrand ==storeShoeStock.shoeBrand){
-          console.log("Found")
-    }
-   });
+    trolley.map(items => {
+      if (items.shoeBrand == storeShoeStock.shoeBrand) {
+        console.log("Found")
+      }
+    });
   }
-      
-    
-   function addToTrolley(){
-     }  
+
+
+  function addToTrolley() {
+  }
 
 
 
-  
+
 
   function addNewStock(brand, Color, Qty, shoesize, Price) {
-   var alreadyExist =false;
-   storeShoeStock.map(current =>{
-  if(current.shoeBrand ===brand&&current.colour===Color&& current.sizeShoe==shoesize){
-       console.log("already exist");
-       let  getQty = parseFloat(current.qty)
-      let  QtyNumber =parseFloat(Qty)
-       current.qty  =getQty +QtyNumber ;
-       alreadyExist =true;
+    var alreadyExist = false;
+    storeShoeStock.map(current => {
+      if (current.shoeBrand === brand && current.colour === Color && current.sizeShoe == shoesize) {
+        console.log("already exist");
+        let getQty = parseFloat(current.qty)
+        let QtyNumber = parseFloat(Qty)
+        current.qty = getQty + QtyNumber;
+        alreadyExist = true;
       }
-   });
-      if(!alreadyExist){
-         storeShoeStock.push({
-      'shoeBrand':brand,
-      'colour':Color,
-      'qty':Qty,
-      'sizeShoe':shoesize,
-      'price': 1500
-    })
-      }
-      return storeShoeStock;
+    });
+    if (!alreadyExist) {
+      storeShoeStock.push({
+        'shoeBrand': brand,
+        'colour': Color,
+        'qty': Qty,
+        'sizeShoe': shoesize,
+        'price': 1500
+      })
     }
- 
-   
+    return storeShoeStock;
+  }
 
 
-  
-  
-   return {
+
+
+
+
+  return {
     stockadd: addNewStock,
     storeMap: stockMap,
     filterBy: filterShoes,
-    cart    :addCart  
+    getFiltered:getSearchedItems,
+    cart: addCart
   }
 }
 
@@ -127,8 +105,8 @@ function addStock() {
     colorElem.value,
     stockQtyElem.value,
     sizeElem.value,
-     150
-    );
+    150
+  );
 
   localStorage.setItem('shoppingBasket', JSON.stringify(shoe_Catalogue.storeMap()));
 }
@@ -137,25 +115,25 @@ function addStock() {
 addStockBtn.addEventListener('click', addStock);
 
 // Search Items 
-searchShoesBtn.addEventListener('click',function(){
-   
-  SearchItems={
-    shoeBrand:shoeSelect.value,
-    colour   : colorSelect.value,
-    sizeShoe : ShoeSizeSelect.value
-   }
-   if(colorSelect.value==""){
+searchShoesBtn.addEventListener('click', function () {
+
+  SearchItems = {
+    shoeBrand: shoeSelect.value,
+    colour: colorSelect.value,
+    sizeShoe: ShoeSizeSelect.value
+  }
+  if (colorSelect.value == "") {
     delete selectedItems.colour;
   }
 
-  if(ShoeSizeSelect.value==""){
+  if (ShoeSizeSelect.value == "") {
     delete selectedItems.sizeShoe;
   }
-  if(shoeSelect.value==""){
+  if (shoeSelect.value == "") {
     delete selectedItems.shoeBrand;
   }
-    console.log(shoe_Catalogue.filterBy(SearchItems));
-    console.log(SearchItems);
+  console.log(shoe_Catalogue.filterBy(SearchItems));
+  console.log(SearchItems);
 });
 
 
--- test/Shoe_catalogue.test.js
+++ test/Shoe_catalogue.test.js
@@ -1,23 +1,77 @@
 describe('The Add Shoes into stock', function() {
-    it('should display   ', function() {
-      var storedItems= [];
-      var shoeCatalogue = ShoeCatalouge(storedItems);
-      shoe_Catalogue.stockadd(
+    it('should display shoe in the shoe stock list if it does not exist in a stock list  ', function() {
+      var shoes = ShoeCatalouge();
+      shoes.stockadd(
         "New Balance","Blue","7","4", 1500);
    
-      assert.deepEqual(shoe_Catalogue.storeMap(),
+      assert.deepEqual(shoes.storeMap(),
       [
-      {shoeBrand: "Adidas", colour: "Blue", qty: "6", sizeShoe: "7", price: 1500},
-      {shoeBrand: "Adidas", colour: "White", qty: "6", sizeShoe: "6", price: 1500},
-      {shoeBrand: "Nike", colour: "White", qty: "1", sizeShoe: "5", price: 1500},
-      {shoeBrand: "Nike", colour: "Blue", qty: "4", sizeShoe: "6", price: 1500},
-      {shoeBrand: "Nike", colour: "Blue", qty: "42", sizeShoe: "5", price: 1500},
-      {shoeBrand: "Nike", colour: "Blue", qty: "12", sizeShoe: "5", price: 1500},
-      {shoeBrand: "Le coq", colour: "Black", qty: 109, sizeShoe: "5", price: 1500},
-      {shoeBrand: 3, colour: "White", qty: "1", sizeShoe: "5", price: 1500},
       {shoeBrand: 'New Balance', colour: "Blue", qty: "7", sizeShoe: "4", price: 1500}]
     );
-
- 
 });
+
+it('should display increased shoe QTY if shoe already exist  ', function() {
+    var shoes = ShoeCatalouge();
+    shoes.stockadd(
+      "New Balance","Blue","7","4", 1500);
+      shoes.stockadd(
+    "New Balance","Blue","4","4", 1500);
+    assert.deepEqual(shoes.storeMap(),
+    [{shoeBrand: 'New Balance', colour: "Blue", qty: 11, sizeShoe: "4", price: 1500}]
+  );
+});
+});
+
+
+describe('Filterby selected brandName,BrandColor,BrandSize', function() {
+    it(' filterBy Brand Name ', function() {
+      var shoes = ShoeCatalouge();
+      shoes.stockadd("New Balance","Blue","7","4", 1500);
+      shoes.filterBy({
+        shoeBrand:"New Balance"
+      });
+      assert.deepEqual(shoes.getFiltered(),
+      [{shoeBrand: 'New Balance', colour: "Blue", qty: "7", sizeShoe: "4", price: 1500}]);
+});
+
+it('filterBy Color', function() {
+    var shoes = ShoeCatalouge();
+    shoes.stockadd("New Balance","Blue","7","4", 1500);
+    shoes.stockadd("Nike","Blue","5","4", 1500);
+    shoes.stockadd("Nike","White","4","4", 1500);
+    shoes.filterBy({colour:"Blue"});
+    assert.deepEqual(shoes.getFiltered(),
+    [{shoeBrand: 'New Balance', colour: "Blue", qty: "7", sizeShoe: "4", price: 1500},
+      {shoeBrand: 'Nike', colour: "Blue", qty: "5", sizeShoe: "4", price: 1500}]    
+  );
+});
+
+it('filterBy Size', function() {
+    var shoes = ShoeCatalouge();
+    shoes.stockadd("New Balance","Blue","7","7", 1500);
+    shoes.stockadd("Nike","Blue","5","4", 1500);
+    shoes.stockadd("Nike","White","4","4", 1500);
+    shoes.filterBy({sizeShoe:"4"});
+    assert.deepEqual(shoes.getFiltered(),
+    [{shoeBrand: 'Nike', colour: "Blue", qty: "5", sizeShoe: "4", price: 1500},
+      {shoeBrand: 'Nike', colour: "White", qty: "4", sizeShoe: "4", price: 1500}]    
+  );
+});
+
+  it('filterBy BrandName,BrandColor and size', function() {
+    var shoes = ShoeCatalouge();
+    shoes.stockadd("New Balance","Blue","7","7", 1500);
+    shoes.stockadd("Nike","Blue","5","4", 1500);
+    shoes.stockadd("Nike","White","4","4", 1500);
+    shoes.filterBy({shoeBrand:"Nike",colour:"Blue",sizeShoe:"4"});
+    assert.deepEqual(shoes.getFiltered(),
+    [{shoeBrand: 'Nike', colour: "Blue", qty: "5", sizeShoe: "4", price: 1500}
+     ]    
+  );
+});
+
+
+
+
+
 });
